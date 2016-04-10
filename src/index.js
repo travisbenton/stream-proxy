61cfc6ca54d096c992443c4ebbd405a71a4b6e89
Travis Benton <travisbenton@Traviss-MacBook-Pro.local>
2016-04-10 16:56:00 -0500

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import peercast from 'peercast';
 import { MongoClient } from 'mongodb';
 import { tvPowerOn } from './helpers/harmonyHub';
 import { databaseConfig } from './helpers/config';
@@ -10,17 +11,17 @@ const getTorrentHash = (db, cb) => {
 
       // wait 30 seconds for the tv/chromecast
       // to turn on before casting the torrent
-      tvPowerOn(setTimeout(() => {
-        const peercast = require('peercast');
-        peercast(doc.torrent);
-      }, 30 * 1000));
+      tvPowerOn(
+        setTimeout(() => peercast(doc.torrent), 30 * 1000)
+      );
     }
   });
 
-  // we got what we need so clear out database... kinda hacky
+  // we got what we need so clear out database
   db.collection('torrents').deleteMany({}, cb);
 };
 
+// poll database every 5 seconds
 const dbPoll = setInterval(() => {
   const { url } = databaseConfig;
   MongoClient.connect(url, (err, db) => {
